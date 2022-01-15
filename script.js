8a15c02e8bf44c1499c47de557eebbb005636574
Tyrik Williams <tyrik.w@yahoo.com>
2022-01-15 13:06:15 -0500

    added arrays

---
+++ script.js
@@ -0,0 +1,19 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var password=document.getElementById("password")
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var specialchar = ['!', '@', '#', '$', '%', '&', '*', '(', ')', '-', '_', '>', '<', '?',]
+var numbers = ['1', '2', '3', '4', '5', '6', '7','8', '9', '0',]
+// Write password to the #password input
+
+
+ 
+
+
+
+
+// Add event listener to generate button
+document.getElementById("generate").addEventListener("click",fuction(writePassword) {
+
+})
